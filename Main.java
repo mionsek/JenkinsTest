3b3dc8af36e92214aa6aba605db03fa306614ac5
Dawid <mionskowski.dawid@gmail.com>
2023-04-17 11:11:16 +0200

    maven added, jsoup for test

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<String> x = new ArrayList<>();
         System.out.println("Worked");
     }
 }
